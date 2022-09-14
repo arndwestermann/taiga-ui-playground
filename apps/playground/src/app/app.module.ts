ccb2ca7d35a346c1dc07ccacf707b6f7dfd19492
Arnd <arnd.westermann@precess.de>
2022-09-14 16:27:04 +0200

    feat(playground): added routing

--- apps/playground/src/app/app.module.ts
+++ apps/playground/src/app/app.module.ts
@@ -1,15 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 
 import { TuiRootModule, TuiDialogModule, TuiAlertModule } from '@taiga-ui/core';
+import { AppRoutingModule } from './app-routing.module';
 
 const taigaUIModules = [TuiRootModule, TuiDialogModule, TuiAlertModule];
 
 @NgModule({
 	declarations: [AppComponent],
-	imports: [BrowserModule, ...taigaUIModules],
+	imports: [BrowserModule, BrowserAnimationsModule, AppRoutingModule, ...taigaUIModules],
 	providers: [],
 	bootstrap: [AppComponent],
 })
